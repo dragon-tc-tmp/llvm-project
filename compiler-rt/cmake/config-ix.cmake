1b595dc0982e3e5848d800d01829f7bdc6f60e26
Yi Kong <yikong@google.com>
2017-12-18 11:55:00 -0800

    compiler-rt: Expand ALL_FUZZER_SUPPORTED_ARCH to include all Android
    Upstream it declares X86 as the only supported libfuzzer arch, to
    prevent issues with cross compilation. This change adds all the Android
    supported archs to ALL_FUZZER_SUPPORTED_ARCH. We need to figure out a
    way to upstream a proper fix for this.
    
    Bug: 70819574
    Change-Id: I34132b41a1138611afc5c87f26bc4bd0d5c7e1c9
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- compiler-rt/cmake/config-ix.cmake
+++ compiler-rt/cmake/config-ix.cmake
@@ -241,7 +241,7 @@ else()
 endif()
 
 if(OS_NAME MATCHES "Linux")
-  set(ALL_FUZZER_SUPPORTED_ARCH ${X86} ${X86_64} ${ARM64})
+  set(ALL_FUZZER_SUPPORTED_ARCH ${X86} ${X86_64} ${ARM32} ${ARM64} ${MIPS32} ${MIPS64})
 else()
   set(ALL_FUZZER_SUPPORTED_ARCH ${X86_64} ${ARM64})
 endif()
